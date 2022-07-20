bfebfea35d9a7b0b0153fabc2e1465cb2fc6c6f8
thejaythree <jaythree206@gmail.com>
2022-07-19 22:46:11 -0700

    Added in new comments/class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+/*
+ * This will act as the main class that will call on start for the game to initialize
+ */
+
+public class Main {
+	
+}
--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,8 @@
 /*
- * This class will allow us to start the game.
+ * This class will be the starting point for the maze.
+ * Potentially being the main GUI window operator.
+ * The best way to think about this class is the setup to a majority
+ * of the maze and its classes.
  */
 
 public class Start {
