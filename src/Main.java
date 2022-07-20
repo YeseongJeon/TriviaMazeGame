7f20c845ca385c2c3dfb5176d09a653c21d54e30
thejaythree <jaythree206@gmail.com>
2022-07-19 22:50:14 -0700

    Added in one more class for support

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 /*
- * This will act as the main class that will call on start for the game to initialize
+ * This will act as the main class that will call on start for the game to initialize.
+ * This is essentially the heart of the program
  */
 
 public class Main {
