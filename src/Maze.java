0a0a67c2a74fc6c2910d72cc3502087085ee8275
yeseongjeon <yeseongjeon@192.168.68.117>
2022-07-19 20:11:06 -0700

    Edit comments commit

--- src/Maze.java
+++ src/Maze.java
@@ -1,5 +1,14 @@
+/*
+ * This will be an extremely Trivial part of our Trivia Maze Game. 
+ * This class will contain a various number of methods.
+ * Some of them are a method to first create the maze. 
+ * A method to print out that maze that we have just created.
+ * A method that allows for the user to change locations. 
+ * Also, a method that will determine the current state of the user. 
+ * For example, if they have or have not yet completed the game. 
+ * These are some of the main methods that shall be within this class.
+ */
+
 public class Maze {
-	public static void main(String[] args) {
-		int example;
-	}
+	
 }
