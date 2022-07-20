b30d73d172233bb32bf434bc903356b99607874d
yeseongjeon <yeseongjeon@192.168.68.117>
2022-07-19 20:17:18 -0700

    Edit comments commit

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,2 +1,9 @@
+/*
+ * This class will contain all the logic towards the entrances to each room. 
+ * When the entrances are shot and when they can be opened. 
+ * When the entrance should be there and at what times that specific entrance should not be there.
+ */
+
 public class Rooms {
+	
 }
