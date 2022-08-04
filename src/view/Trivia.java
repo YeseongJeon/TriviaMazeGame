24295390c6879e5963e9a611ded7c6782b601a03
Liban Jama <libanjama45@gmail.com>
2022-08-04 09:25:27 -0700

    updates to the trivia class

--- src/view/Trivia.java
+++ src/view/Trivia.java
@@ -1,11 +1,39 @@
 package view;
-
+import model.*;
+import java.io.Serializable;
+import java.util.Scanner;
 /*
  * The Trivia class is responsible for constructing the maze, room, question database &  GUI.
  * This allows the capabilities to incorporate beginning games, call another  game, 
  * save game, load game, open menu, and get the users' decision. 
  */
 
-public class Trivia {
+public class Trivia implements Serializable{
+
+        static long serialVersionUID = 1L;
+        static Scanner scan = new Scanner(System.in);
+        static int steps;
+
+        public static char currPlayer(Maze maze) {
+
+        }
+
+        public static void newGame(Maze maze) {
+
+        }
+
+        public static void startGame() {
+        }
+        public static void saveGame(Maze maze) {
+
+        }
+
+        public static Maze loadGame() {
+
+        }
+
+        public static boolean menu(Maze maze) {
+
+        }
 	
 }
