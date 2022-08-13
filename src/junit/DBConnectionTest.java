b76fb9e7493a7d69b67633a9f6af684f99f1cac8
Liban Jama <libanjama45@gmail.com>
2022-08-13 16:41:50 -0700

    updated test files:
    lkgaks
    aklgj;

--- src/junit/DBConnectionTest.java
+++ src/junit/DBConnectionTest.java
@@ -1,4 +1,22 @@
 package junit;
 
+import org.junit.Test;
+
 public class DBConnectionTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void getTrueFalseQuestions() {
+    }
+
+    @Test
+    public void getShortAnswerQuestions() {
+    }
+
+    @Test
+    public void getMultQuestions() {
+    }
 }
--- src/junit/QuestionTest.java
+++ src/junit/QuestionTest.java
@@ -1,4 +1,22 @@
 package junit;
 
+import org.junit.Test;
+
 public class QuestionTest {
+
+    @Test
+    public void getQuestion() {
+    }
+
+    @Test
+    public void getAnswer() {
+    }
+
+    @Test
+    public void getChoices() {
+    }
+
+    @Test
+    public void testToString() {
+    }
 }
