880ce165097cb711da013f32756a87b04b238b0b
Liban Jama <libanjama45@gmail.com>
2022-08-13 19:48:22 -0700

    DB connection update

--- src/junit/DBConnectionTest.java
+++ src/junit/DBConnectionTest.java
@@ -1,35 +1,6 @@
 package junit;
 
-import model.DBConnection;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
 public class DBConnectionTest {
 
-    @Test
-    public void getInstance() {
-        assertNotEquals(DBConnection.getInstance(), null);
 
-    }
-
-    @Test
-    public void getTrueFalseQuestions() {
-
-        assertNotEquals(DBConnection.getTrueFalseQuestions(), null);
-    }
-
-    @Test
-    public void getShortAnswerQuestions() {
-
-        assertNotEquals(DBConnection.getShortAnswerQuestions(), null);
-    }
-
-    @Test
-    public void getMultQuestions() {
-        assertNotEquals(DBConnection.getMultQuestions(), null);
-        final int q = 6;
-        assertEquals(q, DBConnection.getMultQuestions().size());
-    }
 }
