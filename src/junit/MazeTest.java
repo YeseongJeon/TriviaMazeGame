2dd38d6aaefd17ea28e7a5626e6e43ee05fe421b
jaythree <jaythree206@gmail.com>
2022-08-16 00:03:54 -0700

    Updated Maze and Room Test. Not 100% done yet

--- src/junit/MazeTest.java
+++ src/junit/MazeTest.java
@@ -1,75 +1,99 @@
 package junit;
 
 
+import model.Maze;
 import org.junit.Test;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.Random;
 
 public class MazeTest {
+    Random rand = new Random();
+    int randInt = rand.nextInt(3, 7);
+    Maze myMaze = new Maze(randInt, randInt);
 
     @Test
     public void move() {
+        assertNotEquals(myMaze.move(), null);
     }
 
     @Test
     public void moveDown() {
+        assertNotEquals(myMaze.moveDown(), null);
     }
 
     @Test
     public void moveRight() {
+        assertNotEquals(myMaze.moveRight(), null);
     }
 
     @Test
     public void moveUp() {
+        assertNotEquals(myMaze.moveUp(), null);
     }
 
     @Test
     public void moveLeft() {
+        assertNotEquals(myMaze.moveLeft(), null);
     }
 
     @Test
     public void unlock() {
+        assertNotEquals(myMaze.unlock(), null);
     }
 
     @Test
     public void isLastRoom() {
+        assertNotEquals(myMaze.isLastRoom(), null);
     }
 
     @Test
     public void isCurrentRoomLocked() {
+        assertNotEquals(myMaze.isCurrentRoomLocked(), null);
     }
 
     @Test
     public void isCurrentRoomDoorOpen() {
+        assertNotEquals(myMaze.isCurrentRoomDoorOpen(), true);
     }
 
     @Test
     public void deleteCurrentRoomDoor() {
+        assertNotEquals(myMaze.deleteCurrentRoomDoor(), null);
     }
 
     @Test
     public void getRowIndex() {
+        assertNotEquals(myMaze.getRowIndex(), null);
     }
 
     @Test
     public void getColIndex() {
+        assertNotEquals(myMaze.getColIndex(), null);
     }
 
     @Test
     public void getCurrentAvailableDoors() {
+        assertNotEquals(myMaze.getCurrentAvailableDoors(), null);
     }
 
     @Test
     public void size() {
+        assertNotEquals(myMaze.size(), null);
     }
 
     @Test
     public void getMyDisplayMaze() {
+        assertNotEquals(myMaze.getMyDisplayMaze(), null);
     }
 
     @Test
     public void getMaze() {
+        assertNotEquals(myMaze.getMaze(), null);
     }
 
     @Test
     public void testToString() {
+        assertNotEquals(myMaze.toString(), null);
     }
 }
--- src/junit/RoomTest.java
+++ src/junit/RoomTest.java
@@ -1,38 +1,54 @@
 package junit;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import model.Room;
+import static org.junit.Assert.assertNotEquals;
+
 public class RoomTest {
+    Room myRoom = new Room(new char[]{'S', 'E'});
 
     @Test
     public void statusOfDoors() {
+        assertNotEquals(myRoom.statusOfDoors(), null);
     }
 
     @Test
     public void deleteDoor() {
+        myRoom.deleteDoor('S');
+        assertNotEquals(myRoom.statusOfDoors(), null);
     }
 
     @Test
     public void doorLocked() {
+        assertNotEquals(myRoom.doorLocked(), null);
     }
 
     @Test
     public void openDoor() {
+        assertNotEquals(myRoom.openDoor(), null);
     }
 
     @Test
     public void verify() {
+        assertNotEquals(myRoom.verify(), null );
     }
 
     @Test
     public void getAvailableDoors() {
+        assertNotEquals(myRoom.getAvailableDoors(), null);
     }
 
     @Test
     public void isOpened() {
+        assertNotEquals(myRoom.isOpened(), null);
     }
 
     @Test
     public void testToString() {
+        assertNotEquals(myRoom.toString(), null);
     }
 }
