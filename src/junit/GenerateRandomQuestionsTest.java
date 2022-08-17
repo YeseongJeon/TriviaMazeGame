4a808e370c1c1c4b4f61ee335ed210c01f51b784
Yeseong Jeon <cjhj131@gmail.com>
2022-08-16 21:35:51 -0700

    Update DBConnection.java, GenerateRandomQuestions.java

--- src/junit/GenerateRandomQuestionsTest.java
+++ src/junit/GenerateRandomQuestionsTest.java
@@ -1,18 +1,51 @@
 package junit;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import model.GenerateRandomQuestions;
+import model.Question;
 
 public class GenerateRandomQuestionsTest {
 
     @Test
     public void createQuestion() {
-    }
-
-    @Test
-    public void getRandomSelection() {
+    	GenerateRandomQuestions ranQ = new GenerateRandomQuestions(); //single question
+    	Question question = ranQ.createQuestion();
+    	 assertEquals(true, question != null);
     }
 
     @Test
     public void generateQuestion() {
+    	GenerateRandomQuestions grqTF = new GenerateRandomQuestions(); //single question
+    	Question tOfQuestion = grqTF.generateQuestion("TrueOrFalse");
+    	assertEquals("True or False", tOfQuestion.getChoices());
+    	Set<String> possibleTFAnswers = new HashSet<>();
+    	possibleTFAnswers.add("True");
+    	possibleTFAnswers.add("False");
+    	Assert.assertTrue(possibleTFAnswers.contains(tOfQuestion.getAnswer()));
+    	
+    	GenerateRandomQuestions grqSA = new GenerateRandomQuestions(); //single question
+    	Question shortQuestion = grqSA.generateQuestion("ShortAnswer");
+    	assertEquals("Short Answer", shortQuestion.getChoices());
+    	
+    	assertNotEquals(null, shortQuestion.getAnswer());
+    	
+    	GenerateRandomQuestions grqMC = new GenerateRandomQuestions(); //single question
+    	Question multQuestion = grqMC.generateQuestion("MultipleChoice");
+    	assertNotEquals(null, multQuestion.getChoices());
+    	
+    	Set<String> possibleMultAnswers = new HashSet<>();
+    	possibleMultAnswers.add("1");
+    	possibleMultAnswers.add("2");
+    	possibleMultAnswers.add("3");
+    	possibleMultAnswers.add("4");
+    	Assert.assertTrue(possibleMultAnswers.contains(multQuestion.getAnswer()));
     }
 }
