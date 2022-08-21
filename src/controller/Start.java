bea40fd4315e3f68a8f5ed912be53550bf52f1f3
Liban Jama <libanjama45@gmail.com>
2022-08-20 22:05:31 -0700

    comment updates

--- src/controller/Start.java
+++ src/controller/Start.java
@@ -1,9 +1,6 @@
 package controller;
 /*
- * This class will be the starting point for the maze.
- * Potentially being the main GUI window operator.
- * The best way to think about this class is the setup to a majority
- * of the maze and its classes.
+ *
  * This class will allow us to start the game.
  *
  */
--- src/model/Room.java
+++ src/model/Room.java
@@ -5,8 +5,7 @@ import java.util.*;
 import java.io.*;
 /**
  * This class will contain all the logic towards the entrances to each room.
- * When the entrances are shot and when they can be opened.
- * When the entrance should be there and at what times that specific entrance should not be there.
+ *
  * @author Johnny Heredia III
  * @version 1.0 TCSS 360(Summer 2022)
  */
